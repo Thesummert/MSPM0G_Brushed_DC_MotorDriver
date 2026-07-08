e70e224792a27af2a54c3f309df1f731bb819729
Thesummert <3599170972@qq.com>
2026-07-08 21:58:35 +0800

    feat: 添加编码器基础代码

--- User/Device/Inc/qei_encoder.h
+++ User/Device/Inc/qei_encoder.h
@@ -14,7 +14,12 @@ typedef struct {
   uint8_t multiplier;     // 倍频数(通常为4)
   float dt;               // 间隔时间
   float omega;            // 角速度
-};
+  float radio;            // 减速比
+  _Bool is_inited;        // 是否初始化
+} EF_QEI_Encoder_t;
+
+_Bool EF_Device_QEI_Encoder_Init(EF_QEI_Encoder_t *self, EF_BSP_TimerQEI_t *qei,
+                                 uint32_t ppr, uint8_t multiplier, float radio);
 
 #ifdef __cplusplus
 }
--- User/Device/Src/qei_encoder.c
+++ User/Device/Src/qei_encoder.c
@@ -1,3 +1,26 @@
 #include "qei_encoder.h"
+#include "mcu_config.h"
+#include <stdbool.h>
+#include <string.h>
+#include <sys/reent.h>
+
+_Bool EF_Device_QEI_Encoder_Init(EF_QEI_Encoder_t *self, EF_BSP_TimerQEI_t *qei,
+                                 uint32_t ppr, uint8_t multiplier, float radio)
+{
+    if (self == NULL || qei == NULL) {
+        RTT_Print(0, "Null pointer error in qei encoder init \r\n");
+        return false;
+    }
+
+    memset(self, 0, sizeof(EF_QEI_Encoder_t));
+    self->multiplier = multiplier;
+    self->radio = radio;
+    self->pulse_per_rev = ppr;
+    self->qei = qei;
+
+    self->is_inited = true;
+
+    return true;
+}
 
 
